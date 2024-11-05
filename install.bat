b5f5f80c475eed591c1ebd0c8f44afe58e20afc4
Jeffreytsai1004 <jeffreytsai1004@gmail.com>
2024-11-05 19:29:17 +0800

    Update

--- install.bat
+++ install.bat
@@ -1,6 +1,3 @@
-@ECHO OFF
-SETLOCAL EnableDelayedExpansion
-
 @REM SET BASE PATHS
 SET "ROOT_DIR=%~dp0"
 SET "ROOT_DIR=%ROOT_DIR:~0,-1%"
@@ -8,7 +5,7 @@ SET "micromamba=%ROOT_DIR%\micromamba.exe"
 SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 
 @REM CREATE BASE ENVIRONMENT
-@CALL "%micromamba%" create -n gaussianhaircut_base python==3.10.14 git==2.41.0 git-lfs==3.2.0 -c conda-forge -r "%~dp0\" -y || (@ECHO Error: Failed to create base environment && EXIT /B 1)
+@CALL "%micromamba%" create -n gaussianhaircut_base python==3.10.14 git==2.41.0 git-lfs==3.2.0 -c conda-forge -r "%~dp0\" -y
 @CALL "%micromamba%" shell init --shell cmd.exe --prefix "%~dp0\"
 @CALL "%MAMBABAT%" activate gaussianhaircut_base
 @CALL SET ROOT_DIR=%~dp0
@@ -43,7 +40,7 @@ SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 @CALL %micromamba% deactivate
 
 @REM GAUSSIAN SPLATTING HAIR
-@CALL "%micromamba%" create -n gaussian_splatting_hair python==3.10.14 git==2.41.0 git-lfs==3.2.0 -c pytorch -c conda-forge -c defaults -c anaconda -c fvcore -c iopath -c bottler -c nvidia -r "%~dp0\" -y || (@ECHO Error: Failed to create gaussian_splatting_hair environment && EXIT /B 1)
+@CALL "%micromamba%" create -n gaussian_splatting_hair python==3.10.14 git==2.41.0 git-lfs==3.2.0 -c pytorch -c conda-forge -c defaults -c anaconda -c fvcore -c iopath -c bottler -c nvidia -r "%~dp0\" -y
 @CALL "%MAMBABAT%" activate gaussian_splatting_hair
 @CALL SET ROOT_DIR=%~dp0
 @CALL SET PROJECT_DIR=%ROOT_DIR%\GaussianHaircut
@@ -62,7 +59,7 @@ SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 @CALL %micromamba% deactivate
 
 @REM MATTE ANYTHING
-@CALL "%micromamba%" create -n matte_anything python==3.10.14 git==2.41.0 git-lfs==3.2.0 pytorch==2.0.0 pytorch-cuda==11.8 torchvision==0.15.1 tensorboard==2.15.0 timm==0.5.4 opencv==4.5.3 mkl==2024.0 setuptools==58.2.0 easydict wget scikit-image gradio==3.46.1 fairscale supervision==0.22.0 -c pytorch -c nvidia -c conda-forge -r "%~dp0\" -y || (@ECHO Error: Failed to create matte_anything environment && EXIT /B 1)
+@CALL "%micromamba%" create -n matte_anything python==3.10.14 git==2.41.0 git-lfs==3.2.0 pytorch==2.0.0 pytorch-cuda==11.8 torchvision==0.15.1 tensorboard==2.15.0 timm==0.5.4 opencv==4.5.3 mkl==2024.0 setuptools==58.2.0 easydict wget scikit-image gradio==3.46.1 fairscale supervision==0.22.0 -c pytorch -c nvidia -c conda-forge -r "%~dp0\" -y
 @CALL "%MAMBABAT%" activate matte_anything
 @CALL SET PROJECT_DIR=%ROOT_DIR%\GaussianHaircut
 @CALL SET BLENDER_DIR="C:\Program Files\Blender Foundation\Blender 3.6\"
@@ -88,7 +85,7 @@ SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 @CALL %micromamba% deactivate
 
 @REM OPENPOSE
-@CALL "%micromamba%" create -n openpose python==3.10.14 git==2.41.0 git-lfs==3.2.0 cmake=3.20 opencv-python protobuf glog boost h5py numpy make -c conda-forge -r "%~dp0\" -y || (@ECHO Error: Failed to create openpose environment && EXIT /B 1)
+@CALL "%micromamba%" create -n openpose python==3.10.14 git==2.41.0 git-lfs==3.2.0 cmake=3.20 opencv-python protobuf glog boost h5py numpy make -c conda-forge -r "%~dp0\" -y
 @CALL "%MAMBABAT%" openpose
 @CALL git submodule update --init --recursive --remote
 @CALL SET PROJECT_DIR=%ROOT_DIR%\GaussianHaircut
@@ -100,13 +97,13 @@ SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 @IF NOT EXIST %OPENPOSE_DIR%\build 
 @CALL mkdir %OPENPOSE_DIR%\build
 @CALL cd %OPENPOSE_DIR%\build
-@CALL cmake %OPENPOSE_DIR% -DBUILD_PYTHON=true -DUSE_CUDNN=off -DCMAKE_INSTALL_PREFIX="%CD%/install" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/11.8" || (@ECHO Error: CMake configuration failed && EXIT /B 1)
-@CALL cmake --build . --config Release || (@ECHO Error: Build failed && EXIT /B 1)
+@CALL cmake %OPENPOSE_DIR% -DBUILD_PYTHON=true -DUSE_CUDNN=off -DCMAKE_INSTALL_PREFIX="%CD%/install" -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/11.8"
+@CALL cmake --build . --config Release
 @CALL cd %ROOT_DIR%
 @CALL %micromamba% deactivate
 
 @REM PIXIE
-@CALL "%micromamba%" create -n pixie-env python=3.8 pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 fvcore pytorch3d==0.7.5 kornia matplotlib -c pytorch -c nvidia -c fvcore -c conda-forge -c pytorch3d -r "%~dp0\" -y || (@ECHO Error: Failed to create pixie-env && EXIT /B 1)
+@CALL "%micromamba%" create -n pixie-env python=3.8 pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 fvcore pytorch3d==0.7.5 kornia matplotlib -c pytorch -c nvidia -c fvcore -c conda-forge -c pytorch3d -r "%~dp0\" -y
 @CALL "%MAMBABAT%" activate pixie-env
 @CALL SET PROJECT_DIR=%ROOT_DIR%\GaussianHaircut
 @CALL SET BLENDER_DIR="C:\Program Files\Blender Foundation\Blender 3.6\"
@@ -116,7 +113,7 @@ SET "MAMBABAT=%ROOT_DIR%\condabin\micromamba.bat"
 @CALL SET DATA_PATH=%PROJECT_DIR%\data\raw
 @CALL %micromamba% activate pixie-env
 @CALL pip install pyyaml==5.4.1
-@CALL pip install -e %PROJECT_DIR%\ext\face-alignment || (@ECHO Error: Failed to install face-alignment && EXIT /B 1)
+@CALL pip install -e %PROJECT_DIR%\ext\face-alignment
 @CALL %micromamba% deactivate
 
 @ECHO All installations completed successfully.
